0e9db1a79bc57beefafdd9217b05407ec7291650
Jean Souza <jean.co.souza@gmail.com>
2026-02-08 22:10:19 -0300

    Register number keys 0-9 for dev mode unlock on TV remote

--- js/main.js
+++ js/main.js
@@ -471,6 +471,16 @@ try {
   tizen.tvinputdevice.registerKey('MediaStop');
   tizen.tvinputdevice.registerKey('ChannelUp');
   tizen.tvinputdevice.registerKey('ChannelDown');
+  tizen.tvinputdevice.registerKey('0');
+  tizen.tvinputdevice.registerKey('1');
+  tizen.tvinputdevice.registerKey('2');
+  tizen.tvinputdevice.registerKey('3');
+  tizen.tvinputdevice.registerKey('4');
+  tizen.tvinputdevice.registerKey('5');
+  tizen.tvinputdevice.registerKey('6');
+  tizen.tvinputdevice.registerKey('7');
+  tizen.tvinputdevice.registerKey('8');
+  tizen.tvinputdevice.registerKey('9');
 } catch (e) {}
 
 document.addEventListener('keydown', function (e) {
